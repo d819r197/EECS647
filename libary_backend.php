0c20dbb99c3a4de9fd2ef2bff6f876af0273d176
d819r197 <d819r197@ku.edu>
2019-04-11 10:15:16 -0500

    saving work

--- libary_backend.php
+++ libary_backend.php
@@ -2,6 +2,7 @@
 $servername = "mysql.eecs.ku.edu";
 $username = "rblake";
 $password = "chah9riL";
+$dbname = "rblake";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password);
@@ -10,19 +11,32 @@ $conn = new mysqli($servername, $username, $password);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
-echo "Connected successfully";
+echo "Connected successfully <br>";
 
 $sql = $_POST["query"];
-$result = $conn->query($sql);
+echo $sql;
+#$result = $conn->query($sql);
 
-if ($result->num_rows > 0) {
+$result = mysql_query($sql);
+
+if (!$result) {
+    echo '<br>Could not run query: ' . mysql_error() . "<br>";
+    #exit;
+}
+
+$row = mysql_fetch_row($result);
+
+echo "cell 1: " . $row[0]; // 42
+echo "cell 2: " . $row[1]; // the email value
+
+/*if ($result->num_rows > 0) {
     // output data of each row
     while($row = $result->fetch_assoc()) {
         echo "Row: " . $row . "<br>";
     }
 } else {
     echo "0 results";
-}
+}*/
 $conn->close();
 
 ?>
