18d2161a97e79818a485946e7a89a7d16bc723fb
d819r197 <d819r197@ku.edu>
2019-04-11 09:41:47 -0500

    connected index.html to db

--- libary_backend.php
+++ libary_backend.php
@@ -1,4 +1,18 @@
 <?php
+$servername = "mysql.eecs.ku.edu";
+$username = "rblake";
+$password = "chah9riL";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+echo "Connected successfully";
+
+
   $user = $_POST["user"];
   $pass = $_POST["pass"];
   $total = 0;
